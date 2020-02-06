9b12df9d7c83b39abb52068a9307411936db6136
Rajan G <rajangrg1@hotmail.com>
2020-02-06 18:05:53 +1300

    Initial files

---
+++ AcademicTranscriptAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AcademicTranscriptAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AcademicTranscriptMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AcademicTranscriptMVC.MvcApplication" Language="C#" %>
