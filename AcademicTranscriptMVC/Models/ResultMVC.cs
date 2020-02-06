50b18d4d2029bae7e87fffc2376c4d2dc7c98818
Rajan G <rajangrg1@hotmail.com>
2020-02-06 19:05:32 +1300

    Adding ResultMVC - model

---
+++ AcademicTranscriptMVC/Models/ResultMVC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcademicTranscriptMVC.Models
+{
+    public class ResultMVC
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public Nullable<int> CourseLevel { get; set; }
+        public string Credit { get; set; }
+        public string Grade { get; set; }
+        public Nullable<int> Year { get; set; }
+    }
+}
