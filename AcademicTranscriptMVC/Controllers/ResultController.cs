b8f536448dad74c1de811a3d37840e091ae4e7c1
Rajan G <rajangrg1@hotmail.com>
2020-02-06 19:43:53 +1300

    Fetching api/result data into view

--- AcademicTranscriptMVC/Controllers/ResultController.cs
+++ AcademicTranscriptMVC/Controllers/ResultController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using AcademicTranscriptMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,7 +13,11 @@ namespace AcademicTranscriptMVC.Controllers
         // GET: Result
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<ResultMVC> resultList;
+            HttpResponseMessage fetchResult = GlobalVariable.WebApiClient.GetAsync("Result").Result;
+            resultList = fetchResult.Content.ReadAsAsync<IEnumerable<ResultMVC>>().Result;
+
+            return View(resultList);
         }
     }
 }
---
+++ AcademicTranscriptMVC/GlobalVariable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace AcademicTranscriptMVC
+{
+    public static class GlobalVariable
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+
+        static GlobalVariable()
+        {
+            //Web API project url
+            WebApiClient.BaseAddress = new Uri("https://localhost:44319/api/");
+            WebApiClient.DefaultRequestHeaders.Clear();
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+    }
+}
